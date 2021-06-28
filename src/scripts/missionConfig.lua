f3bd0e5579e8f1a7515e715f46495f6adfc03566
David Pierron <dpierron@gmail.com>
2021-06-28 13:50:27 +0200

    Multiple changes : - changes to configuration (voice of Hound Elint controller) - set Mi-24P default skin to VEAF

--- src/scripts/missionConfig.lua
+++ src/scripts/missionConfig.lua
@@ -519,7 +519,7 @@ if veafHoundElint then
             },
             controller = {
                 freq = 282.225,
-                voice = true
+                voiceEnabled = true
             }
         }
     )
