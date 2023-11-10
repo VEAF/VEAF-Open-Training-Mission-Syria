098fa9ff4951ec1670175fc42fa3500d233e9ae3
David Pierron <dpierron@gmail.com>
2023-11-10 18:27:57 +0100

    Reset the TADL (datalink) on all A-10C2

--- build-deploy-test.cmd
+++ build-deploy-test.cmd
@@ -1,6 +1,7 @@
 @echo off
 set NOPAUSE=true
 set SKIP_WEATHER=true
+rem set LUA_SCRIPTS_DEBUG_PARAMETER=-trace
 set DYNAMIC_SCRIPTS_PATH="d:\dev\_VEAF\VEAF-Mission-Creation-Tools"
 set DYNAMIC_LOAD_SCRIPTS=
 call build.cmd
